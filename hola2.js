f011431a998f9d787e37f3b4920ee75f0c8f5260
electrocomputacion <leocuello86@hotmail.com>
2018-06-22 21:17:16 -0300

    nueva linea

--- hola2.js
+++ hola2.js
@@ -1,8 +1,13 @@
-meteor.Meteor.call("meteorMethod", dataObject, function(error, result){ 
+meteor.Meteor.call("meteorMethod", dataObject, function(error, result){
   if(error){
     console.log("error", error);
   }
   if(result){
+
+  }
+});
+Meteor.methods({
+  methodName:function(){ 
 
   }
 });
