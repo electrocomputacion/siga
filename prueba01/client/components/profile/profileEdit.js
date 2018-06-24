390cb609c00d41dd9dc6c771646742e3c87e19ed
electrocomputacion <leocuello86@hotmail.com>
2018-06-24 18:52:20 -0300

    Actualica lo de modificar el perfil, ya funciona, es solo para probarlo hay q definir los campos

--- prueba01/client/components/profile/profileEdit.js
+++ prueba01/client/components/profile/profileEdit.js
@@ -2,14 +2,21 @@ import {mongo} from 'meteor/mongo';
 import  {Meteor} from 'meteor/meteor'
 
 Template.profileEdit.events({
-  "submit #formModificar": function(event, template){
+
+  "submit #formModificarProfile": function(event, template){   //captura el evento submit del formulario
      event.preventDefault();
      const target=event.target;
-     var ingresoNombre=target.nombreApellido.value;
-     Meteor.user.update({_id:Meteor.userId()}, {$set:{
-       "profile.name":ingresoNombre
-
-     }});
-
+     var ingresoNombre=target.name.value;     //se alamcena el contenido del input en una variable
+     var ingresoApellido=target.surname.value;
+     var ingresoDireccion=target.address.value;
+console.log("entro a la funcion");
+     //var nombre = "hola";
+     Meteor.users.update(Meteor.userId(), {$set:{       //actualizo los datos de la coleccion
+       "profile.name":ingresoNombre}});
+       Meteor.users.update(Meteor.userId(), {$set:{       //esto se puede hacer de una sola, ver como
+         "profile.surname":ingresoApellido}});
+         Meteor.users.update(Meteor.userId(), {$set:{
+           "profile.address":ingresoDireccion}});
+Router.go('profile');                                   // le digo que una vez termine vuelva al profile
   },
 });
